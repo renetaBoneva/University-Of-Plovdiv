69eb260d5087f81598197e68881d5dbc5c141da4
Ренета Бонева <reneta.boneva@abv.bg>
2024-03-21 22:46:05 +0200

    Fill all tables

--- Databases/Final project/create-db-tables.sql
+++ Databases/Final project/create-db-tables.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE Fitness
+﻿CREATE DATABASE Fitness
 	COLLATE Cyrillic_General_CI_AS;
 
 GO 
@@ -21,7 +21,7 @@ CREATE TABLE workouts
 	price DECIMAL(6,2)
 
 	CONSTRAINT pk_workouts PRIMARY KEY (workout_id),
-	CONSTRAINT CHK_MuscleGroups CHECK (leading_muscle_group IN ('abs', 'legs', 'arms', 'back', 'upper body', 'booty')),
+	CONSTRAINT CHK_MuscleGroups CHECK (leading_muscle_group IN ('корем', 'крака', 'ръце', 'гръб', 'горна част', 'глутеус'))
 )
 
 CREATE TABLE trainers
@@ -47,6 +47,7 @@ CREATE TABLE workouts_exercises
 	workout_id INT NOT NULL ,
 	exercise_id INT NOT NULL,
 	
+	CONSTRAINT pk_workout_id_exercise_id PRIMARY KEY (workout_id, exercise_id),
 	CONSTRAINT fk_workouts_exercises_workouts FOREIGN KEY (workout_id) REFERENCES workouts(workout_id) ON DELETE CASCADE,
 	CONSTRAINT fk_workouts_exercises_exercises FOREIGN KEY (exercise_id) REFERENCES exercises(exercise_id) ON DELETE CASCADE,
 )
@@ -56,6 +57,7 @@ CREATE TABLE trainers_workouts
 	workout_id INT NOT NULL,
 	trainer_id INT NOT NULL,
 	
+	CONSTRAINT pk_workout_id_trainer_id PRIMARY KEY (workout_id, trainer_id),
 	CONSTRAINT fk_trainers_workouts_workouts FOREIGN KEY (workout_id) REFERENCES workouts(workout_id) ON DELETE CASCADE,
 	CONSTRAINT fk_trainers_workouts_trainers FOREIGN KEY (trainer_id) REFERENCES trainers(trainer_id) ON DELETE CASCADE,
 )
@@ -64,13 +66,13 @@ CREATE TABLE appointments
 (
 	appointment_id INT IDENTITY(5000, 1),
 	workout_id INT,
-	client_id INT,
+	client_id INT NOT NULL,
 	trainer_id INT NOT NULL,
 	appointent_time DATETIME NOT NULL,
 	isValidAppointment BIT DEFAULT 1,
 	-- NULL-> didnt happened, 0-> happened, 1-> up comming
 	
-	CONSTRAINT pk_appointments PRIMARY KEY (appointment_id),
+	CONSTRAINT pk_appointments PRIMARY KEY (client_id, appointent_time),
 	CONSTRAINT fk_appointments_workouts FOREIGN KEY (workout_id) REFERENCES workouts(workout_id),
 	CONSTRAINT fk_appointments_trainers FOREIGN KEY (trainer_id) REFERENCES trainers(trainer_id) ON DELETE CASCADE,
 	CONSTRAINT fk_appointments_clients FOREIGN KEY (client_id) REFERENCES clients(client_id) ON DELETE CASCADE,
