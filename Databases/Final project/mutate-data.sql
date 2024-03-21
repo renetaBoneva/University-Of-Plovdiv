68c07a12816a87211ba55ca81330e9bf95c06715
Ренета Бонева <reneta.boneva@abv.bg>
2024-03-21 22:47:33 +0200

    Start operating with filed data

---
+++ Databases/Final project/mutate-data.sql
@@ -0,0 +1,7 @@
+﻿-- Сменете часа за тренировка на клиент с номер 4004 с треньор номер 3004 от '2024-03-25 15:30:00'
+-- за '2024-03-25 17:30:00'.
+UPDATE appointments
+SET appointent_time='2024-03-25 17:30:00'
+WHERE client_id='4004' AND trainer_id='3004' AND appointent_time='2024-03-25 15:30:00'
+
+SELECT * FROM appointments
