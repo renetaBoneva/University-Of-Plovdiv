ca64e3d189270824b61dcd677e374349a096103d
Ренета Бонева <reneta.boneva@abv.bg>
2024-03-29 17:17:44 +0200

    Working on my final project for the Database Class

--- Databases/Final project/mutate-data.sql
+++ Databases/Final project/mutate-data.sql
@@ -1,7 +1,25 @@
-﻿-- Сменете часа за тренировка на клиент с номер 4004 с треньор номер 3004 от '2024-03-25 15:30:00'
+﻿GO
+USE Fitness;
+
+-- Сменете часа за тренировка на клиент с номер 4004 с треньор номер 3004 от '2024-03-25 15:30:00'
 -- за '2024-03-25 17:30:00'.
 UPDATE appointments
 SET appointent_time='2024-03-25 17:30:00'
 WHERE client_id='4004' AND trainer_id='3004' AND appointent_time='2024-03-25 15:30:00'
 
-SELECT * FROM appointments
+-- Да се изведе информация за всички клиенти със записани часове
+SELECT *
+FROM clients
+WHERE client_id IN (SELECT client_id FROM appointments);
+
+-- Добавете упражнение 'Кошнички' с номер 1000 към тренировъчна 
+-- програма 'Тренировка 6' с номер 2006;
+INSERT INTO workouts_exercises(workout_id, exercise_id)
+VALUES
+	(2006, 1000);
+
+-- Изрийте клиент номер 4006 от релация 'Клиенти'
+DELETE FROM clients
+WHERE client_id='4006'
+
+-- Изведете таблица на всички тренировки с включените в тях упражнения
